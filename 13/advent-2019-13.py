97475457b22097511d12c570e0154a4cb913fab1
David J Heberle <davidjheberle@gmail.com>
2019-12-13 17:40:47 -0500

    add solution for day 13

--- 13/advent-2019-13.py
+++ 13/advent-2019-13.py
@@ -1,4 +1,5 @@
 import getopt, sys
+import os
 
 def read_input():
     fullCmdArguments = sys.argv
@@ -48,14 +49,15 @@ def set_memory(program, index, value):
     buffer_memory(program, index)
     program[index] = value
 
-def intcode(program, program_input=[], ptr=0, rel_base=0):
+def intcode(program, program_input=[], ptr=0, rel_base=0, input_function=None):
+    if ptr is None: return None, None, None
     output, params, num_params = 0, {}, (3, 3, 1, 1, 2, 2, 3, 3, 1)
     while get_memory(program, ptr) != 99:
         opcode = get_memory(program, ptr) % 100
         params = get_params(program, ptr, num_params[opcode - 1], rel_base)
         if opcode == 1: set_memory(program, params[3], get_memory(program, params[1]) + get_memory(program, params[2]))
         elif opcode == 2: set_memory(program, params[3], get_memory(program, params[1]) * get_memory(program, params[2]))
-        elif opcode == 3: set_memory(program, params[1], program_input.pop(0))
+        elif opcode == 3: set_memory(program, params[1], program_input.pop(0) if program_input else input_function())
         elif opcode == 4: output = get_memory(program, params[1])
         elif opcode == 5 and get_memory(program, params[1]) or opcode == 6 and not get_memory(program, params[1]): ptr = get_memory(program, params[2]) - 3
         elif opcode == 7: set_memory(program, params[3], 1 if get_memory(program, params[1]) < get_memory(program, params[2]) else 0)
@@ -65,77 +67,34 @@ def intcode(program, program_input=[], ptr=0, rel_base=0):
         if opcode == 4: return output, ptr, rel_base
     return output, None, None
 
-def read_panel(position, canvas):
-    if position in canvas.keys():
-        if canvas[position] == '#': return 1
-        elif canvas[position] == '.': return 0
-    return 0
-
-def paint_panel(instruction, position, canvas):
-    paints = ('.', '#')
-    canvas[position] = paints[instruction]
-
-def turn(input, index, directions):
-    if input == 0:
-        index -= 1
-    elif input == 1:
-        index += 1
-    if index < 0:
-        index = len(directions) - 1
-    elif index > len(directions) - 1:
-        index = 0
-    return index, directions[index]
-
-def hull_painting_robot(program, start_color):
-    canvas, position, directions, direction_index = {}, (0, 0), [(0, 1), (1, 0), (0, -1), (-1, 0)], 0
-    canvas[position] = start_color
+def part1(program):
+    print("Part 1")
+    block_count = 0
     ptr, rel_base, inputs = 0, 0, []
     while ptr is not None:
-        inputs.append(read_panel(position, canvas))
-        output, ptr, rel_base = intcode(program, inputs, ptr, rel_base)
-        paint_panel(output, position, canvas)
-        if ptr is None: break
-        output, ptr, rel_base = intcode(program, inputs, ptr, rel_base)
-        direction_index, _ = turn(output, direction_index, directions)
-        position = (position[0] + directions[direction_index][0], position[1] + directions[direction_index][1])
-    return canvas
+        _, ptr, rel_base = intcode(program, inputs, ptr, rel_base)
+        _, ptr, rel_base = intcode(program, inputs, ptr, rel_base)
+        tile_id, ptr, rel_base = intcode(program, inputs, ptr, rel_base)
+        if tile_id == 2: block_count += 1
+    return block_count
 
-def print_canvas(canvas):
-    canvas_array = []
-    x_min, x_max, y_min, y_max = None, None, None, None
-    for key in canvas:
-        if x_min is None: x_min = key[0]
-        elif key[0] < x_min: x_min = key[0]
-        if x_max is None: x_max = key[0]
-        elif key[0] > x_max: x_max = key[0]
-        if y_min is None: y_min = key[1]
-        elif key[1] < y_min: y_min = key[1]
-        if y_max is None: y_max = key[1]
-        elif key[1] > y_max: y_max = key[1]
-    start = (x_min, y_min)
-    x_length = x_max - x_min + 1
-    y_length = y_max - y_min + 1
-    for _ in range(y_length):
-        canvas_array.append(['.'] * x_length)
-    for key in canvas:
-        canvas_array[key[1]-start[1]][key[0]-start[0]] = canvas[key]
-    final_string = ''
-    for y in range(y_length):
-        for x in range(x_length):
-            final_string += canvas_array[y_length-y-1][x]
-        final_string += '\n'
-    return final_string
-
-def part1(raw_input):
-    print("Part 1")
-    canvas = hull_painting_robot(get_program(raw_input), '.')
-    return len(canvas)
-
-def part2(raw_input):
+def part2(program):
     print("Part 2")
-    canvas = hull_painting_robot(get_program(raw_input), '#')
-    return print_canvas(canvas)
+    ball_x = paddle_x = 0
+    score = 0
+    set_memory(program, 0, 2)
+    ptr, rel_base, inputs = 0, 0, []
+    joystick_input = lambda: (ball_x > paddle_x) - (ball_x < paddle_x)
+    while ptr is not None:
+        x, ptr, rel_base = intcode(program, inputs, ptr, rel_base, joystick_input)
+        y, ptr, rel_base = intcode(program, inputs, ptr, rel_base, joystick_input)
+        tile_id, ptr, rel_base = intcode(program, inputs, ptr, rel_base, joystick_input)
+        if ptr is not None:
+            paddle_x = x if tile_id == 3 else paddle_x
+            ball_x = x if tile_id == 4 else ball_x
+            score = tile_id if (x, y) == (-1, 0) else score
+    return score
 
 raw_input = read_input()
-print(part1(raw_input))
-print(part2(raw_input))
+print(part1(get_program(raw_input)))
+print(part2(get_program(raw_input)))
