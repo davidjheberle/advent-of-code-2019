0a2bedc5bc554e22f6c7690b1e6d7928052b77bb
David J Heberle <davidjheberle@gmail.com>
2019-12-15 19:22:43 -0500

    start solution for day 14

---
+++ 14/advent-2019-14.py
@@ -0,0 +1,37 @@
+import getopt, sys
+import os
+
+def read_input():
+    fullCmdArguments = sys.argv
+    argumentList = fullCmdArguments[1:]
+    unixOptions = "f:"
+    gnuOptions = "file="
+
+    try:
+        arguments, _ = getopt.getopt(argumentList, unixOptions, gnuOptions)
+    except getopt.error as err:
+        print(str(err))
+        sys.exit(2)
+
+    fileName = None
+    for currentArgument, currentValue in arguments:
+        if currentArgument in ("-f", "--file"):
+            fileName = currentValue
+            print("Reading %s" % fileName)
+
+    file = open(fileName)
+    raw_input = file.read()
+    file.close()
+    return raw_input
+
+def part1(raw_input):
+    print("Part 1")
+    return None
+
+def part2(raw_input):
+    print("Part 2")
+    return None
+
+raw_input = read_input()
+print(part1(raw_input))
+print(part2(raw_input))
