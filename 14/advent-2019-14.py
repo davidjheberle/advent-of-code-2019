bf750c7c8f58a37e9eb139d8d126303c79ab924d
David J Heberle <davidjheberle@gmail.com>
2019-12-16 14:00:03 -0500

    add day 14 solution

--- 14/advent-2019-14.py
+++ 14/advent-2019-14.py
@@ -1,5 +1,9 @@
 import getopt, sys
-import os
+import re
+from collections import defaultdict
+
+PATTERN = re.compile(r'(\d+) (\w+)')
+MAX = 1000000000000
 
 def read_input():
     fullCmdArguments = sys.argv
@@ -24,14 +28,54 @@ def read_input():
     file.close()
     return raw_input
 
-def part1(raw_input):
+def parse(lines):
+    recipes = {}
+    for line in lines:
+        ingredients, last = {}, None
+        for match in PATTERN.finditer(line):
+            if last is not None:
+                key, n = last
+                ingredients[key] = n
+            last = match.group(2), int(match.group(1))
+        key, n = last
+        recipes[key] = n, ingredients
+    return recipes
+
+def create(recipes, element, quantity):
+    materials = defaultdict(int)
+    materials[element] = quantity
+    while True:
+        try:
+            key, n = next((key, n) for key, n in materials.items() if key != 'ORE' and n > 0)
+            m, ingredients = recipes[key]
+            x = (n + m - 1) // m
+            materials[key] -= m * x
+            for k, v in ingredients.items():
+                materials[k] += x * v
+        except StopIteration:
+            break
+    return materials['ORE']
+
+def part1(lines):
     print("Part 1")
-    return None
+    recipes = parse(lines)
+    return create(recipes, 'FUEL', 1)
 
-def part2(raw_input):
+def part2(lines):
     print("Part 2")
-    return None
+    recipes = parse(lines)
+    good, bad = MAX // create(recipes, 'FUEL', 1), None
+    while bad is None or good < bad - 1:
+        quantity = 2 * good if bad is None else (good + bad) // 2
+        ores = create(recipes, 'FUEL', quantity)
+        if ores < MAX:
+            good = quantity
+        elif ores > MAX:
+            bad = quantity
+        else:
+            return MAX
+    return good
 
-raw_input = read_input()
-print(part1(raw_input))
-print(part2(raw_input))
+lines = read_input().splitlines()
+print(part1(lines))
+print(part2(lines))
